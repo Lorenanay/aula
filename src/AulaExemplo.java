5e7854e1da4c469ca98e2b6a7969a43626b2bd06
Lorenanay <85141693+Lorenanay@users.noreply.github.com>
2021-05-31 22:28:12 -0300

    Mudança nos valores

--- src/AulaExemplo.java
+++ src/AulaExemplo.java
@@ -3,8 +3,8 @@ public class AulaExemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git é muito bom");
-		double x=3;
+		System.out.println("Ricardo é chato");
+		double x=7;
 		double y=2;
 		double multiplicacao=x*y;
 		System.out.println("resultado " + multiplicacao);
