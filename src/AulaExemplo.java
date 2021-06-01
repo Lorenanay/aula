b1a07c66397638626629f95fecee36557de2e8c1
Lorenanay <85141693+Lorenanay@users.noreply.github.com>
2021-05-31 22:25:37 -0300

    Adicionado Multiplicação

--- src/AulaExemplo.java
+++ src/AulaExemplo.java
@@ -4,6 +4,10 @@ public class AulaExemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git é muito bom");
+		double x=3;
+		double y=2;
+		double multiplicacao=x*y;
+		System.out.println("resultado " + multiplicacao);
 	}
 
 }
